4fd0850243b12beac9c65a54bfcef7af9cfbde9c
pengquan wang <pengquan.wang@dasudian.com>
2017-02-23 19:57:02 +0800

    Timer's variable changes

--- project_template/timer/UCOSII/DatahubTimer.c
+++ project_template/timer/UCOSII/DatahubTimer.c
@@ -5,8 +5,8 @@
 char expired(Timer* timer)
 {
 	unsigned int now = OSTimeGet();
-	int res = timer->end_time - now;
-	//printf("时间是否过期，expired:end_time(%d) - now(%d) = %d\r\n", timer->end_time, now, res);
+	int res = timer->end - now;
+	//printf("时间是否过期，expired:end(%d) - now(%d) = %d\r\n", timer->end, now, res);
 	return res <= 0;
 }
 
@@ -19,28 +19,28 @@ void countdown_ms(Timer* timer, unsigned int timeout)
 		count = 1;
 	}
 	now = OSTimeGet();
-	timer->end_time = now + count;
-	//printf("定时器往后加:countdown_ms:now=%d, count=%d, end_time=%d\r\n", now, count, timer->end_time);
+	timer->end = now + count;
+	//printf("定时器往后加:countdown_ms:now=%d, count=%d, end=%d\r\n", now, count, timer->end);
 }
 
 // 将定时器从当前时间往后加timeout秒
 void countdown(Timer* timer, unsigned int timeout)
 {
 	unsigned int count = timeout * OS_TICKS_PER_SEC;
-	timer->end_time = OSTimeGet() + count;
-	//printf("countdown:timer->end_time = %d\r\n", timer->end_time);
+	timer->end = OSTimeGet() + count;
+	//printf("countdown:timer->end = %d\r\n", timer->end);
 }
 
 // 返回还有多少ms超时
 int left_ms(Timer* timer)
 {
 	unsigned int now = OSTimeGet();
-	int res = timer->end_time - now;
+	int res = timer->end - now;
 	//printf("还有多少ms超时，left_ms:res = %d\r\n", res);
 	return (res < 0) ? 0 : res * (1000 / OS_TICKS_PER_SEC);
 }
 
 void InitTimer(Timer* timer)
 {
-	timer->end_time = 0;
+	timer->end = 0;
 }
